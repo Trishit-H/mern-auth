fccaee54c8d976f097f3d0aadd37731c7c893cdb
Trishit Hazra <hazratrishit@gmail.com>
2024-06-07 23:03:16 +0530

    added authRouter and middleware
    Added authRouter and the express.json middleware that lets the server accept json responses.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const connectDB = require('./db.js');
+const authRouter = require('./routes/auth.route.js');
 
 const app = express();
 
 const PORT = process.env.PORT || 5002;
 
+// middleware to accept json response
+app.use(express.json());
+
 // connect to database
 connectDB();
+
+// set up api routes
+app.use('/api/auth', authRouter)
 
 // start server
 app.listen(PORT, () => {
