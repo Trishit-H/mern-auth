0dde5b203270ac5765d634511e8b4058fdd4262c
Trishit Hazra <hazratrishit@gmail.com>
2024-06-09 13:15:34 +0530

    Created handleSignup function and installed bcryptjs
    Created the function that sign ups a user to the app. We destructure username, email and password from the request body and then we hash the password using the hashSync method of bcryptjs. Weuse bcryptjs instead of bcrypt becuase the latter one creates some issues while using on production.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,32 @@
 const User = require('../models/user.model.js');
+const bcryptjs = require('bcryptjs');
 
-async function handleSignup() {
+async function handleSignup(req, res) {
+    const { username, email, password } = req.body;
 
+    try {
+        // hash the password
+        const hashedPassword = await bcryptjs.hashSync(password, 10);
+
+        // create a new user using the hashed password
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        // save the user to database
+        await newUser.save();
+
+        // send response if user is created successfully
+        res.status(201).json({
+            success: true,
+            msg: 'User created successfully',
+            data: newUser
+        });
+    } catch (err) {
+        return res.status(500).json({ error: err.message })
+    }
 };
 
 module.exports = {
