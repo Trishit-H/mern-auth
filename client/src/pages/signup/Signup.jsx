d5dff0d87919d65b9db364c124f5ab064a143e33
Trishit Hazra <hazratrishit@gmail.com>
2024-06-09 20:06:21 +0530

    Created sign up page ui and added some basic styling

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,9 +1,25 @@
 import './signup.css';
 
+import { Link } from 'react-router-dom';
+
 function Signup() {
     return (
-        <div>Signup</div>
+        <div className='signup-wrapper'>
+            <h1 className='title'>Sign Up</h1>
+            <form className="form-wrapper">
+                <input type="text" placeholder='Username' />
+                <input type="email" placeholder='Email' />
+                <input type="password" placeholder='Password' />
+                <button>Sign Up</button>
+            </form>
+            <div className="footer">
+                <p>Have an account?</p>
+                <Link to='/sign-in'>
+                    <span>Sign In</span>
+                </Link>
+            </div>
+        </div>
     )
 }
 
-export default Signup
+export default Signup;
