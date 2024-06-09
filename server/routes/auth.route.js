d6e8b2cc2e67d5194e6b21f595f28705c3481fb0
Trishit Hazra <hazratrishit@gmail.com>
2024-06-09 23:02:36 +0530

    Added route for signning in users.

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {
-    handleSignup
+    handleSignup,
+    handleSignin
 } = require('../controllers/auth.controller.js');
 
 const router = express.Router();
 
 router.post('/signup', handleSignup);
+router.post('/signin', handleSignin);
 
 module.exports = router;
