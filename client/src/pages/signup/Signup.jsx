984195dc0d29e93d356d670d9a5bd915f033fabb
Trishit Hazra <hazratrishit@gmail.com>
2024-06-09 20:35:30 +0530

    Added a `handleChange` function in the Signup component to manage form data state. This function updates the `formData` state with the values entered in the form fields for username, email, and password.
    Changes:
    - Introduced `useState` to hold `formData` as an object.
    - Added `handleChange` event handler to update `formData` dynamically as user inputs data.
    - Modified input fields to use `handleChange` for capturing and storing input values.
    
    This enhancement facilitates the management of form data within the Signup component,
    making it ready for form submission and further validation processes.

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,15 +1,41 @@
 import './signup.css';
 
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Signup() {
+
+    const [formData, setFormData] = useState({});
+
+    const handleChange = (e) => {
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [e.target.name]: e.target.value
+        }));
+    };
+
     return (
         <div className='signup-wrapper'>
             <h1 className='title'>Sign Up</h1>
             <form className="form-wrapper">
-                <input type="text" placeholder='Username' />
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
+                <input
+                    type="text"
+                    placeholder='Username'
+                    name='username'
+                    onChange={handleChange}
+                />
+                <input
+                    type="email"
+                    placeholder='Email'
+                    name='email'
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder='Password'
+                    name='password'
+                    onChange={handleChange}
+                />
                 <button>Sign Up</button>
             </form>
             <div className="footer">
