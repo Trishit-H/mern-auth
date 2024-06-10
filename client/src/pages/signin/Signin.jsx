b4d5b16855a162c897aa4fb81f0070509798150a
Trishit Hazra <hazratrishit@gmail.com>
2024-06-11 00:00:21 +0530

    Add Signin component with form and error handling
    - Implemented the Signin component for user authentication.
    - Added form state management using useState for handling user input.
    - Integrated fetch API for sending sign-in requests to the backend.
    - Included loading state to provide user feedback during the sign-in process.
    - Added error handling to display an error message if sign-in fails.
    - Utilized useNavigate for redirecting users upon successful sign-in.
    - Added basic form fields for email and password with change handlers.
    - Included a link to the Sign-Up page for users without an account.
    - Applied basic styling with an external CSS file 'signin.css'.

--- client/src/pages/signin/Signin.jsx
+++ client/src/pages/signin/Signin.jsx
@@ -1,9 +1,91 @@
 import './signin.css';
 
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 function Signin() {
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({});
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleChange = (e) => {
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            setLoading(true)
+            setError(false);
+
+            const response = await fetch('api/auth/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await response.json();
+
+            setLoading(false);
+
+            if (data.success) {
+                navigate('/');
+                return
+            }
+
+            if (!data.success) {
+                setError(true);
+                return
+            }
+        } catch (err) {
+            setLoading(false);
+            setError(true)
+        }
+    }
+
     return (
-        <div>Signin</div>
+        <div onSubmit={handleSubmit} className='signin-wrapper'>
+            <h1 className='title'>Sign In</h1>
+            {
+                error && <p className='err-msg'>Something went wrong!</p>
+            }
+            <form className="form-wrapper">
+                <input
+                    type="email"
+                    placeholder='Email'
+                    name='email'
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder='Password'
+                    name='password'
+                    onChange={handleChange}
+                />
+                <button disabled={loading}>
+                    {
+                        loading ? 'Signing in...' : 'Sign In'
+                    }
+                </button>
+            </form>
+            <div className="footer">
+                <p>Don't have an account?</p>
+                <Link to='/sign-up'>
+                    <span>Sign Up</span>
+                </Link>
+            </div>
+        </div>
     )
 }
 
-export default Signin
+export default Signin;
