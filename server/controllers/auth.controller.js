6851103dbcdf09806c97ecd2d597ef585b5ad371
Trishit Hazra <hazratrishit@gmail.com>
2024-06-10 11:22:02 +0530

    Add user sign-in functionality
    - Implemented `handleSignin` function to handle user authentication.
    - Validates email and password against the stored user records.
    - Utilizes bcryptjs for password comparison.
    - Returns appropriate errors for invalid credentials using the custom error handler.
    - Ensures proper error handling and user response on sign-in attempt.
    
    This commit completes the basic user authentication flow, complementing the existing sign-up functionality.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user.model.js');
 const bcryptjs = require('bcryptjs');
+const customErrorHandler = require('../utils/customError.js');
 
 async function handleSignup(req, res, next) {
     const { username, email, password } = req.body;
@@ -29,6 +30,36 @@ async function handleSignup(req, res, next) {
     }
 };
 
+
+
+async function handleSignin() {
+    const { email, password } = req.body;
+
+    try {
+        // check if an user exist with the email provided
+        const validUser = await User.findOne({ email });
+
+        // if not then return an error as response using the custoom error handler function and the error middleware
+        if (!validUser) {
+            return next(customErrorHandler(404, 'Invalid Credentials!'))
+        }
+
+        // if the email is valid then we check the password
+        const isPasswordValid = await bcryptjs.compareSync(password, validUser.password);
+
+        // if password is not correct we return an error
+        if (!isPasswordValid) {
+            return next(customErrorHandlder(401, 'Invalid Credentials!'))
+        }
+
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+
 module.exports = {
-    handleSignup
+    handleSignup,
+    handleSignin
 };
